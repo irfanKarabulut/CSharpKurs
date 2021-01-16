b667f36d58c948adff9a8569459c9dcaeeefee3c
irfanKarabulut <irfankarabulut86@gmail.com>
2021-01-16 23:42:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpKurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
